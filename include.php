fe34585e0cb90aa2521c0c08e7722d65b0ab2562
John Kershaw <wardrox@gmail.com>
2013-01-11 12:52:11 +0000

    Include will conditionally include object files to increase speed

--- include.php
+++ include.php
@@ -6,7 +6,7 @@
 // Exception handling
 include('functions/exceptions.php');
 
-// include needed files
+//configuration files
 include('config.php');
 
 // Control (function) files 
@@ -21,23 +21,28 @@ include('functions/tracker.fnc.php');
 include('functions/cache.fnc.php');
 include('functions/sessions.fnc.php');
 
+//recaptcha functions
+include('functions/recaptcha/recaptchalib.php');
+
 // Database (function) files
 include('functions/database/questions.db.fnc.php');
 include('functions/database/answers.db.fnc.php');
 include('functions/database/reports.db.fnc.php');
 
-// Model (object) files
-include('objects/database_derbytest.obj.php');
-include('objects/question.obj.php');
-include('objects/answer.obj.php');
-include('objects/response.obj.php');
-include('objects/report.obj.php');
-include('objects/user.obj.php');
-include('objects/term.obj.php');
-include('objects/test.obj.php');
-
-// Extra files
-include("functions/phpmailer/class.phpmailer.php");
-include('functions/recaptcha/recaptchalib.php');
+// Model (object) files autoload, phpMailer is also Autoloaded as used rarely
+function __autoload($classname) 
+{
+	switch (strtolower($classname)) 
+	{	
+		case "phpmailer":
+			$filename = "functions/phpmailer/class.phpmailer.php";
+			break;	
+		
+		default:
+			$filename = "objects/". $classname .".obj.php";
+			break;		
+	}	
+    include($filename);
+}
 
 ?>
