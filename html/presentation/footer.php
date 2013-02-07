f8d8ceae3a274dee4824561d243bd47e7fce4af4
John Kershaw <wardrox@gmail.com>
2013-02-07 18:28:38 +0000

    Moved Facebook code to end of the page

--- html/presentation/footer.php
+++ html/presentation/footer.php
@@ -140,6 +140,16 @@
 		} 
 		?>	
 		
+		
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=131848900255414";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+				
 			<div class="facebook_wrap_wide">
 				<div class="fb-like" data-href="http://rollerderbytestomatic.com" data-send="false" data-width="500" data-show-faces="true"></div>
 			</div>
