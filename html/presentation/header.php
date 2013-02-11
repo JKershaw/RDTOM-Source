188756e5eb807cf87370f1485ddb370b681f3d44
John Kershaw <wardrox@gmail.com>
2013-02-11 13:56:05 +0000

    Facebook code updated to be non-blocking. jQuery script include moved to after CSS for faster loading.

--- html/presentation/header.php
+++ html/presentation/header.php
@@ -15,8 +15,6 @@
 		
 		<title>Roller Derby Test O'Matic</title>
 		
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-   		
 		<link rel="stylesheet" href="<?echo get_CSS_URL(); ?>" type="text/css" />
 		<link rel="stylesheet" href="<?echo get_CSS_URL("print"); ?>" type="text/css" media="print">
 		
@@ -35,26 +33,29 @@
 		}
 		?>
 	
-	<?php 
-	if (!is_admin_page())
-	{
-		?>
-		<script type="text/javascript">
-		
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-431964-17']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		
-		</script>	
 		<?php 
-	} 
-	?>
+		if (!is_admin_page())
+		{
+			?>
+			<script type="text/javascript">
+			
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', 'UA-431964-17']);
+			  _gaq.push(['_trackPageview']);
+			
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			
+			</script>	
+			<?php 
+		} 
+		?>
+	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+   		
 	</head>
 	
 	<body>
