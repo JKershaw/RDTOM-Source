b9ba48ac8b78267387ab5d08cb591de751a9bd62
John Kershaw <wardrox@gmail.com>
2013-03-09 16:21:58 +0000

    report question debug moved to function file

--- html/functions/functions.php
+++ html/functions/functions.php
@@ -3,6 +3,12 @@
 
 function report_question()
 {
+	if (!$_POST)
+	{
+		header( 'Location: ' . get_site_URL()) ;
+		exit;
+	}
+	
 	global $report_string, $error_string, $url_array;
 	
 	if ($_POST['report_question_ID'] && (strtolower(trim($_POST['report_extra'])) == "derby"))
