2ae82d06dbcb77e56855667785fde4260be32c1f
John Kershaw <wardrox@gmail.com>
2013-04-08 11:24:45 +0100

    Profile stats are cached for 5 mins when loaded

--- app/presentation/profile.php
+++ app/presentation/profile.php
@@ -104,6 +104,8 @@ if (is_logged_in())
 	</form>
 	
 	<div class="layout_box" id="layout_box_stats">
+	
+		<p>Your stats may take a few minutes to update.</p>
 		<?php echo return_stats_user_totals() ?>
 		
 		<?php echo return_stats_user_section_totals() ?>
