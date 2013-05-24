5fce6e5cdf41d2eb14dd84f13d7bb2df51ff6229
John Kershaw <wardrox@gmail.com>
2013-05-24 19:44:18 +0100

    Min Skills app privacy policy page made

---
+++ app/presentation/minimumskills.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+ * Roller Derby Test O'Matic
+ * Created by John Kershaw
+ * 
+ * Built to help Roller Derby players learn the rules
+ */
+
+
+// display the page
+set_page_subtitle("Turn left and pass your minimum skills.");
+include("header.php");
+?>
+	<h3>Minimum Skills App</h3>
+	<p>
+		Available on Android.
+	</p>
+	
+	<h3>Privacy Policy</h3>
+	<p>	
+		The app will only remember information as long as necessary, and won't disclose personal information to outside sources without your explicit permission. The app will only collect personal information by lawful and fair means. Personal data collected will be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date. The app will protect your personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification. 
+	</p>
+<?php 
+include("footer.php");
+?>
