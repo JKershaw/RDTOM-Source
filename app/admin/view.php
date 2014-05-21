414cdcf981b96b043dd9917e59e6279937ab33cb
Sausage <wardrox@gmail.com>
2014-05-21 11:33:32 +0100

    splitting admin view

--- app/admin/view.php
+++ app/admin/view.php
@@ -16,78 +16,9 @@
 	</p>
 
 	<?php include(__DIR__ . "/tabs/editQuestion.php"); ?>
-		
-	
-	<div class="layout_box" id="layout_box_all_questions" style="display:none;">
-		
-		
-		<p>Search: <input type="text" id="questions_search" name="questions_search"/> <a onClick="get_search_questions_list();">Search</a></p>
-		<p>Section: <input type="text" id="filter_section" name="filter_section"/></p>
-		<p>Rule set: 
-			<a class="current_ruleset_selector current_ruleset_selector_WFTDA5" onClick="change_current_ruleset('WFTDA5');">WFTDA5</a> 
-			<a class="current_ruleset_selector current_ruleset_selector_WFTDA6" onClick="change_current_ruleset('WFTDA6');">WFTDA6</a> 
-			<a class="current_ruleset_selector current_ruleset_selector_WFTDA6_Draft" onClick="change_current_ruleset('WFTDA6_Draft');">WFTDA6_Draft</a></p>
-		
-		<p><a onclick="$('.extra_all').toggle();">Toggle all</a></p>
-		
-		<p id="viewalllink"><a onclick="$('#viewalllink').hide(); $('#viewalllist').show(); get_all_questions_list();">Load all questions</a></p>
-		
-		<p id="viewalllist" style="display:none">
-		</p>
-		
-		<script type="text/javascript">
-		var filter_string;
-		var current_ruleset = "question_string";
 
-		function change_current_ruleset(new_ruleset)
-		{
-			current_ruleset = '.' + new_ruleset;
-			$('.current_ruleset_selector').css( "font-weight" , "normal");
-			$('.current_ruleset_selector_' + new_ruleset).css( "font-weight" , "bold");
-			filterQuestions(true);
-		}
+	<?php include(__DIR__ . "/tabs/allQuestions.php"); ?>
 		
-		function filterQuestions(var_force)
-		{
-			var current_section = $("#filter_section").val();
-
-			// Do we need to filter?
-			if ((filter_string != current_section) || var_force)
-			{
-				filter_string = current_section;
-					
-				// remove the training full stop if there is one
-				if (current_section.substr(current_section.length - 1) == ".")
-				{
-					current_section = current_section.slice(0, - 1);
-				}
-
-				// hide everything, then decide what to show $('.question_string').hide();
-				$('.question_string').hide();
-				
-				// filter by section
-				if (current_section == "")
-				{
-					// no section specified, so show the current section
-					$(current_ruleset).show();
-				}
-				else
-				{
-					// a section is given
-					current_section = '.section_' + current_section.replace(/\./g, "_");
-					$(current_section + current_ruleset).show();
-				}
-				
-			}
-		}
-
-		$(document).ready(function(){
-		    var intervalID = setInterval(function(){
-		    	filterQuestions(false)
-		    }, 100); // 100 ms check
-		});
-		</script>
-	</div>
 	
 	<div class="layout_box" id="layout_box_recompute" style="display:none;">
 	
