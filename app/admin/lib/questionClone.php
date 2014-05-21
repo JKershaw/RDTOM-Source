69e22ff6e8cf486c6d7c447ad249e58219020342
Sausage <wardrox@gmail.com>
2014-05-21 16:30:28 +0100

    refactoring and allowing WFTDA 7 update

--- app/admin/lib/questionClone.php
+++ app/admin/lib/questionClone.php
@@ -1,26 +1,15 @@
 <?php
 
-$questionToBeCloned = get_question_from_ID($_GET['clone_question']);
+$questionToBeCloned = get_question_from_ID($question_id);
 
-// Adding a new question
-// try to save the question
-$new_question_id = add_question($questionToBeCloned->get_Text(), $questionToBeCloned->get_Section(), $questionToBeCloned->get_Notes());
+$new_question_id = clone_question($questionToBeCloned);
 
-// save all the answers
+$message.= "The question has been cloned.";
 
-$answersToBeCloned = $questionToBeCloned->get_Answers(100);
-
-foreach ($answersToBeCloned as $id => $answer) {
-	add_answer($new_question_id, $answer->get_Text(), $answer->is_correct());
+if ($_GET['updateToWFTDA7']) {
+	updateToWFTDA7($questionToBeCloned);
+	$message.= "Updated to WFTDA 7";
 }
-
-$message.= "Cloned question and answers saved! ";
-
-// build new relationships
-foreach ($questionToBeCloned->get_terms() as $term_ID => $term) {
-	$mydb->add_relationship($new_question_id, $term->get_ID());
-}
-$message.= "Relationships Built! ";
 
 // save a comment
 $comment_text = "Question Cloned from \n\n" . $questionToBeCloned;
@@ -30,4 +19,29 @@ $comment = new comment(-1, $user->get_ID(), $new_question_id, gmmktime(), $comme
 
 // save the comment
 set_comment($comment);
+
+function clone_question($questionToBeCloned) {
+
+	// save the new question
+	$new_question_id = add_question($questionToBeCloned->get_Text(), $questionToBeCloned->get_Section(), $questionToBeCloned->get_Notes());
+	
+	// save all the answers
+	$answersToBeCloned = $questionToBeCloned->get_Answers(100);
+	
+	foreach ($answersToBeCloned as $id => $answer) {
+		add_answer($new_question_id, $answer->get_Text(), $answer->is_correct());
+	}
+	
+	// save the tags
+	foreach ($questionToBeCloned->get_terms() as $term_ID => $term) {
+		$mydb->add_relationship($new_question_id, $term->get_ID());
+	}
+
+	return $new_question_id;
+}
+
+function updateToWFTDA7($question){
+	$WFTDA7_term_ID = "23";
+	$mydb->add_relationship($question->get_ID, $WFTDA7_term_ID);
+}
 ?>
