d3e7a38004f1a11f1ce266b578d7d546c138b072
Sausage <wardrox@gmail.com>
2014-05-21 16:45:52 +0100

    when updating a question to WFTDA 7 the first section number is decreased

--- app/admin/lib/questionClone.php
+++ app/admin/lib/questionClone.php
@@ -1,5 +1,4 @@
 <?php
-
 $questionToBeCloned = get_question_from_ID($_GET['clone_question']);
 
 $new_question_id = clone_question($mydb, $questionToBeCloned);
@@ -21,7 +20,7 @@ $comment = new comment(-1, $user->get_ID(), $new_question_id, gmmktime(), $comme
 set_comment($comment);
 
 function clone_question($mydb, $questionToBeCloned) {
-
+	
 	// save the new question
 	$new_question_id = add_question($questionToBeCloned->get_Text(), $questionToBeCloned->get_Section(), $questionToBeCloned->get_Notes());
 	
@@ -36,21 +35,36 @@ function clone_question($mydb, $questionToBeCloned) {
 	foreach ($questionToBeCloned->get_terms() as $term_ID => $term) {
 		$mydb->add_relationship($new_question_id, $term->get_ID());
 	}
-
+	
 	return $new_question_id;
 }
 
-function updateToWFTDA7($mydb, $user, $question){
+function updateToWFTDA7($mydb, $user, $question) {
+	
+	// add the WFTDA 7 tag
 	$WFTDA7_term_ID = "23";
 	$mydb->add_relationship($question->get_ID(), $WFTDA7_term_ID);
+	
+	// decriment the first digit of the section number
+	$section = $question->get_Section();
+	
+	if (strpos($section, '.') !== false) {
+		$section_parts = explode(".", $section);
+		if (intval($section_parts[0]) > 0) {
+			$section_parts[0] = intval($section_parts[0]) - 1;
+		}
 
-
+		$section = implode(".", $section_parts);
+	}
+	
+	edit_question($question->get_ID(), $question->get_Text(), $section, $question->get_Notes());
+	
 	// save a comment
-	$comment_text = "Question run through the update script to WFTDA7";
-
+	$comment_text = "Question run through the update script to tag it WFTDA7";
+	
 	// make a new comment
 	$comment = new comment(-1, $user->get_ID(), $question->get_ID(), gmmktime(), $comment_text, QUESTION_CHANGED);
-
+	
 	// save the comment
 	set_comment($comment);
 }
