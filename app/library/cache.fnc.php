8bdf9d081f65733ffafb641a9de02b84608e6a93
Sausage <wardrox@gmail.com>
2014-07-09 19:25:04 +0100

    Tidying cache function code

--- app/library/cache.fnc.php
+++ app/library/cache.fnc.php
@@ -1,8 +1,6 @@
 <?php
-function cache_set($key, $string, $timeout = 86400)
-{
-	
-	// generate the data to save
+function cache_set($key, $string, $timeout = 86400) {
+
 	$key = preg_replace("/[^a-zA-Z0-9]/", "", $key);
 	
 	$data['timestamp'] = time();
@@ -17,9 +15,8 @@ function cache_set($key, $string, $timeout = 86400)
 
 	$fh = fopen($filename, 'w');
 
-	// save it
-	$stringData = serialize($data);
-	fwrite($fh, $stringData);
+	$serializedData = serialize($data);
+	fwrite($fh, $serializedData);
 	
 	fclose($fh);
 
@@ -28,44 +25,37 @@ function cache_set($key, $string, $timeout = 86400)
 	
 }
 
-function cache_get($key)
-{
+function cache_get($key) {
 		
 	$key = preg_replace("/[^a-zA-Z0-9]/", "", $key);
 	
 	@$fh = fopen("../filecache/" . $key, 'r');
-	if (!$fh)
-	{
+	if (!$fh) {
 		return false;
 	}
+
 	@$theData = fread($fh, filesize("../filecache/" . $key));
+	
 	fclose($fh);
 	
-	if (!$theData)
-	{
+	if (!$theData) {
 		return false;
 	}
 	
 	$theData = unserialize($theData);
-	
-	//save_log("cache", "GET " . $key . ": Timestamp = " . $theData['timestamp'] . ", Timeout = " . $theData['timeout'] . ", Current timestamp = " . time());
-		
+
 	if ($_GET['nocache'] || (($theData['timestamp'] + $theData['timeout']) < time()))
 	{
-		//save_log("cache", "GET " . $key . " Out of date");
-		
 		cache_delete($key);
 		return false;
 	}
 	else 
 	{
-		//save_log("cache", "GET " . $key . " Valid");
 		return unserialize($theData['data']);
 	}
 }
 
-function cache_delete($key)
-{
+function cache_delete($key) {
 	$key = preg_replace("/[^a-zA-Z0-9]/", "", $key);
 	@unlink("../filecache/" . $key);
 }
