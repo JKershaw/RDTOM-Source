9f0fcc4027639ffa1da4bf3f7ab368cee5b60a37
Sausage <wardrox@gmail.com>
2014-09-21 22:19:47 +0100

    Tinkering with config

--- tests/acceptance/GenerateRulesTestCept.php
+++ tests/acceptance/GenerateRulesTestCept.php
@@ -5,7 +5,6 @@ $I->amOnPage('');
 $I->see('Generate a Rules Test');
 $I->click('Generate a Rules Test');
 
-$I->amOnPage('/test');
 $I->see("Click the button to randomly generate an online rules test");
 
 $I->click("Generate Rules Test");
