6f5f957249f17033200ea37eb858e23322073492
Sausage <wardrox@gmail.com>
2014-09-22 13:40:40 +0100

    Adding more API 0.2 tests

--- tests/api/API0_2Cept.php
+++ tests/api/API0_2Cept.php
@@ -48,18 +48,18 @@ $I->see('"status_code":"200"');
 $I->amOnPage('/api/0.2/jsonp/question/?callback=my_function&ID=704&developer=test&application=testingScript');
 $I->see('my_function ({"api_version":"0.2","status_code":"200","resource":{"question":{"id":"704","text":"True or False: \"Sausage Roller\" is a really clever and funny name for a Merby (Men\'s Derby) skater.","wftda_link":{},"terms":{"language":"English"},"notes":"NB: A \"sausage roll\" is a tasty snack popular in the UK. It\'s sausage meat rolled and baked in pastry. Yum.","sections":{"section":"N\/A"},"answers":{"answer":[{"id":"3052","text":"False","correct":"false"},{"id":"3051","text":"True","correct":"true"}]}}}}, \'\');');
 
+$I->amOnPage('/api/0.2/JavaScript/question/?ID=704&developer=test&application=testingScript');
+$I->see('<status_code>0</status_code>');
+$I->see('<status_message>No Javascript variable name specified</status_message>');
 
-// // Resource Questions
-// $I->amOnPage('/api/0.1/xml/questions/');
-// $I->see('<status_code>200</status_code>');
+$I->amOnPage('/api/0.2/JavaScript/question/?ID=704&developer=test&application=testingScript&var=var');
+$I->see('var var = {"api_version":"0.2","status_code":"200","resource"');
 
-// $I->amOnPage('/api/0.1/nicexml/questions/');
-// $I->see('&lt;api_version&gt;0.1&lt;/api_version&gt;');
+// Resource Questions
 
-// $I->amOnPage('/api/0.1/json/questions/');
-// $I->see('"status_code":"200"');
+// Resource Statistic 
 
-// $I->amOnPage('/api/0.1/jsonp/questions/?callback=my_function');
-// $I->see('"status_code":"200"');
+// Resource Changes
 
-// ?>
+
+?>
