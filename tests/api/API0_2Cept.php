7d102a90d3bd01cd1d131795ffdb59ed7f2da13c
Sausage <wardrox@gmail.com>
2014-09-22 17:18:50 +0100

    adding more API tests

--- tests/api/API0_2Cept.php
+++ tests/api/API0_2Cept.php
@@ -56,6 +56,21 @@ $I->amOnPage('/api/0.2/JavaScript/question/?ID=704&developer=test&application=te
 $I->see('var var = {"api_version":"0.2","status_code":"200","resource"');
 
 // Resource Questions
+$I->amOnPage('/api/0.2/xml/questions/?developer=test&application=testingScript');
+$I->see('<status_code>200</status_code>');
+
+$I->amOnPage('/api/0.2/nicexml/questions/?developer=test&application=testingScript');
+$I->see('&lt;api_version&gt;0.2&lt;/api_version&gt;');
+
+$I->amOnPage('/api/0.2/json/questions/?developer=test&application=testingScript');
+$I->see('"status_code":"200"');
+
+$I->amOnPage('/api/0.2/jsonp/questions/?callback=my_function&developer=test&application=testingScript');
+$I->see('"status_code":"200"');
+
+// the search parameter
+$I->amOnPage('/api/0.2/json/questions/?developer=test&application=testingScript&search=sausage');
+$I->see('{"api_version":"0.2","status_code":"200","resource":{"questions":{"question":{"id":"704"');
 
 // Resource Statistic 
 
