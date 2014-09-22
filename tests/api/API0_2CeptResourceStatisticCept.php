06f5c61fea05b1d6c98771d4a6e24640e4823ab9
Sausage <wardrox@gmail.com>
2014-09-22 17:31:20 +0100

    All tests for stats

--- tests/api/API0_2CeptResourceStatisticCept.php
+++ tests/api/API0_2CeptResourceStatisticCept.php
@@ -11,6 +11,36 @@ $I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&s
 $I->see('<status_code>200</status_code>');
 $I->see('<id>responses</id>');
 
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=api_calls_hourly');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>api_calls_hourly</id>');
 
-//responses, api_calls_hourly, responses_daily, responses_hourly, responses_minutly, questions, answers, users, unique_IPs
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=responses_daily');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>responses_daily</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=responses_hourly');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>responses_hourly</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=responses_minutly');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>responses_minutly</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=questions');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>questions</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=answers');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>answers</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=users');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>users</id>');
+
+$I->amOnPage('/api/0.2/xml/Statistic/?developer=test&application=testingScript&search=sausage&ID=unique_IPs');
+$I->see('<status_code>200</status_code>');
+$I->see('<id>unique_IPs</id>');
+
 ?>
