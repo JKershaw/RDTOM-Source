dc78fd431975968e42cc980bc57ee071d00d34de
Sausage <wardrox@gmail.com>
2014-09-22 13:01:17 +0100

    Adding more API tests

--- tests/api/API0_1Cept.php
+++ tests/api/API0_1Cept.php
@@ -2,12 +2,16 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Test version 0.1 of the API');
 
+// Resource Question
 $I->amOnPage('/api/0.1/xml/question/');
 $I->see('<status_code>200</status_code>');
 
 $I->amOnPage('/api/0.1/xml/question/?ID=704');
 $I->see('<status_code>200</status_code>');
 $I->see('Sausage Roller');
+
+$I->amOnPage('/api/0.1/jsonp/question/?callback=my_function&ID=704');
+$I->see('my_function ({"api_version":"0.1","status_code":"200","resource":{"question":{"id":"704","text":"True or False: \"Sausage Roller\" is a really clever and funny name for a Merby (Men\'s Derby) skater.","wftda_link":{},"notes":"NB: A \"sausage roll\" is a tasty snack popular in the UK. It\'s sausage meat rolled and baked in pastry. Yum.","sections":{"section":"N\/A"},"answers":{"answer":[{"id":"3052","text":"False","correct":"false"},{"id":"3051","text":"True","correct":"true"}]}}}}, \'\');');
 
 $I->amOnPage('/api/0.1/nicexml/question/');
 $I->see('&lt;api_version&gt;0.1&lt;/api_version&gt;');
@@ -24,6 +28,7 @@ $I->see('"status_code":"200"');
 $I->amOnPage('/api/0.1/jsonp/question/?callback=my_function');
 $I->see('"status_code":"200"');
 
+// Resource Questions
 $I->amOnPage('/api/0.1/xml/questions/');
 $I->see('<status_code>200</status_code>');
 
