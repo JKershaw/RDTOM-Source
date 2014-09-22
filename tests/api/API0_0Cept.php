1253e17c679ebce5ea3ff71edc54a1ab27b05295
Sausage <wardrox@gmail.com>
2014-09-22 12:54:22 +0100

    Adding API tests using PHPBrowser

--- tests/api/API0_0Cept.php
+++ tests/api/API0_0Cept.php
@@ -2,7 +2,5 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Test version 0.0 of the API');
 $I->amOnPage('/api/0.0/xml/question/');
-$XML = $I->getVisibleText();
-codecept_debug($XML);
 $I->see('<status_code>601</status_code>');
 ?>
