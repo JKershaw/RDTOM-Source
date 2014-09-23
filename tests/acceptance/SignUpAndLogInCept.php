846cbaf12bffe06f5b6242665a67d8b5e87e33c2
Sausage <wardrox@gmail.com>
2014-09-23 12:25:07 +0100

    Expanding sign up and log in tests

--- tests/acceptance/SignUpAndLogInCept.php
+++ tests/acceptance/SignUpAndLogInCept.php
@@ -11,4 +11,24 @@ $I->fillField('name', 'testname');
 $I->fillField('password', 'password');
 $I->click('Login');
 $I->see('Name and password combination not found, please try again.');
+
+// sign up
+$I->click('click here to get one');
+$I->see("Sign up");
+$I->fillField('#signup_name', 'testname');
+$I->fillField('#signup_password', 'password');
+$I->click('Sign up');
+$I->see('Your account has been made, please log in now ');
+
+// sign in
+$I->see("Login to your account");
+$I->fillField('name', 'testname');
+$I->fillField('password', 'password');
+$I->click('Login');
+
+// I'm now logged in
+$I->see('You have not answered any questions whilst logged in');
+
+
+
 ?>
