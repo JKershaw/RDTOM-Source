be05462c1afcce99d853e436a1b42ddf55c31876
Sausage <wardrox@gmail.com>
2014-09-24 16:10:07 +0100

    updating tests

--- tests/acceptance/SignUpAndLogInCept.php
+++ tests/acceptance/SignUpAndLogInCept.php
@@ -1,5 +1,9 @@
 <?php 
 $I = new AcceptanceTester($scenario);
+
+$test_name = "testname";
+$test_password = "password";
+
 $I->wantTo('Sign up');
 $I->amOnPage('');
 $I->see('Log in or sign up');
@@ -9,8 +13,8 @@ $I->click('Log in or sign up');
 //incorrect details
 $I->see("Login to your account");
 $I->dontSee("If you already have an account");
-$I->fillField('name', 'testname');
-$I->fillField('password', 'password');
+$I->fillField('name', $test_name);
+$I->fillField('password', $test_password);
 $I->click('Login');
 $I->see('Name and password combination not found, please try again.');
 
@@ -18,15 +22,15 @@ $I->see('Name and password combination not found, please try again.');
 $I->click('click here to get one');
 $I->see("Sign up");
 $I->dontSee("Login to your account");
-$I->fillField('#signup_name', 'testname');
-$I->fillField('#signup_password', 'password');
+$I->fillField('#signup_name', $test_name);
+$I->fillField('#signup_password', $test_password);
 $I->click('Sign up');
 $I->see('Your account has been made, please log in now ');
 
 // sign in
 $I->see("Login to your account");
-$I->fillField('name', 'testname');
-$I->fillField('password', 'password');
+$I->fillField('name', $test_name);
+$I->fillField('password', $test_password);
 $I->click('Login');
 
 // I'm now logged in
