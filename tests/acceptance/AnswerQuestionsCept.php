ebab400807cb5a08b75effe1ff0966a1948f062e
Sausage <wardrox@gmail.com>
2014-09-26 15:01:34 +0100

    Testing streaks and forgetting

--- tests/acceptance/AnswerQuestionsCept.php
+++ tests/acceptance/AnswerQuestionsCept.php
@@ -32,6 +32,41 @@ $I->see('You have a current success rate of 50% (1 correct out of 2)', "#remeber
 $I->see('Wrong!', ".wrong_answer");
 
 // Get a streak going
+$I->click("New Question");
+$I->click(".correct_answer_link");
+$I->click("New Question");
+$I->click(".correct_answer_link");
+$I->click("New Question");
+$I->click(".correct_answer_link");
+$I->click("New Question");
+$I->click(".correct_answer_link");
+$I->click("New Question");
+$I->click(".correct_answer_link");
+
+$I->see('You are on a winning streak of 5', "#remebered_string_p");
+$I->see('You have a current success rate of 85.71% (6 correct out of 7)', "#remebered_string_p");
 
 // Break the streak
+$I->click("New Question");
+$I->click(".wrong_answer_link");
+$I->waitForElementChange('#remebered_string', function(\WebDriverElement $el) {
+    return $el->isDisplayed();
+}, 100);
+
+$I->see('You just ended your streak of 5', "#remebered_string_p");
+$I->see('You have a current success rate of 75% (6 correct out of 8)', "#remebered_string_p");
+
+// Forget and start again
+$I->click("Forget");
+
+$I->dontSee('You have a current success rate of', "#remebered_string_p");
+$I->dontSee('You Win!');
+
+$I->click(".correct_answer_link");
+$I->waitForElementChange('#remebered_string', function(\WebDriverElement $el) {
+    return $el->isDisplayed();
+}, 100);
+
+$I->see('You have a current success rate of 100% (1 correct out of 1)', "#remebered_string_p");
+$I->see('You Win!', ".correct_answer_win");
 ?>
