1892f92b8c6e030511a8e68add8f557b9afbf0d4
Sausage <wardrox@gmail.com>
2014-09-29 16:02:06 +0100

    fixing cron issue

--- html/cron.php
+++ html/cron.php
@@ -1,79 +1,84 @@
-<?php 
+<?php
+
 /*
  * Roller Derby Test O'Matic
  * Created by John Kershaw
- * 
+ *
  * Built to help Roller Derby players learn the rules
- */
+*/
 
 /*
  * This file is loaded every 5 minutes and must remeber which functions it has fired and when
- */
+*/
 
 /*
  * An array holding the Cron jobs
- */
-
-include_once("FileCache");
-
-$cron_tasks = Array ( 
-				Array (
-					"function" => "stats_hourly_posts",
-					"seconds" => 1200),
-				Array (
-					"function" => "response_count_last_hour",
-					"seconds" => 300),
-				Array (
-					"function" => "last_10000_sections",
-					"seconds" => 3600),
-				Array (
-					"function" => "rebuild_sitemap",
-					"seconds" => 86400),
-				Array (
-					"function" => "stats_count_unique_IPs",
-					"seconds" => 3600),
-				Array (
-					"function" => "delete_old_usertokens",
-					"seconds" => 86400),
-				Array (
-					"function" => "archive_responses",
-					"seconds" => 3600),
-				Array (
-					"function" => "unarchive_responses",
-					"seconds" => 6000)
-			);
-
+*/
 
 // include needed files
-include('../app/include.php');
+include ('../app/include.php');
+
+include_once ("FileCache");
+
+$cron_tasks = Array(
+	Array(
+		"function" => "stats_hourly_posts",
+		"seconds" => 1200
+	) ,
+	Array(
+		"function" => "response_count_last_hour",
+		"seconds" => 300
+	) ,
+	Array(
+		"function" => "last_10000_sections",
+		"seconds" => 3600
+	) ,
+	Array(
+		"function" => "rebuild_sitemap",
+		"seconds" => 86400
+	) ,
+	Array(
+		"function" => "stats_count_unique_IPs",
+		"seconds" => 3600
+	) ,
+	Array(
+		"function" => "delete_old_usertokens",
+		"seconds" => 86400
+	) ,
+	Array(
+		"function" => "archive_responses",
+		"seconds" => 3600
+	) ,
+	Array(
+		"function" => "unarchive_responses",
+		"seconds" => 6000
+	)
+);
 
 // process and return the cron request
-try 
-{
+try {
+	
 	// create the database
 	set_up_database();
 	
 	// load the file containing info on all the cron jobs
 	@$cron_tasks_data = unserialize(file_get_contents('../filecache/cron_tasks_data'));
-
-	if ($cron_tasks_data)
-	{
+	
+	if ($cron_tasks_data) {
 		echo "cron_tasks_data loaded <br />";
-	}
-	else
-	{
+	} else {
 		echo "cron_tasks_data not found <br />";
 	}
 	
 	$count = 0;
+	
 	// for each cron job, check if it needs to be fired.
-	foreach ($cron_tasks as $cron_task)
-	{
+	foreach ($cron_tasks as $cron_task) {
+		
 		// check with the $cron_tasks_data to see when this function was last fired
-		if (($cron_tasks_data[$cron_task['function']] < (gmmktime() -  $cron_task['seconds'])) || ($_GET['force']))
-		{
-			if ($_GET['force'] && ($_GET['force'] != $cron_task['function']))
-			{
+		if (($cron_tasks_data[$cron_task['function']] < (gmmktime() - $cron_task['seconds'])) || ($_GET['force'])) {
+			if ($_GET['force'] && ($_GET['force'] != $cron_task['function'])) {
+				
 				//echo "Force: " . $_GET['force'] . "!=" . $cron_task['function'] . "<br />";
 				continue;
 			}
@@ -88,8 +93,7 @@ try
 			echo "Cron completed: " . $cron_task['function'] . "<br />";
 			
 			// only execute one cron job per cycle to keep server load light
-			if ($count > 1)
-			{
+			if ($count > 1) {
 				break;
 			}
 		}
@@ -100,87 +104,85 @@ try
 	
 	echo "cron_tasks_data saved <br />";
 }
-catch (Exception $e) 
-{
+catch(Exception $e) {
 	save_log("error_cron", $e->getMessage());
 }
 
 echo "Cron checked!";
 
-function stats_hourly_posts()
-{
+function stats_hourly_posts() {
 	global $mydb;
 	$fileCache = new FileCache();
 	$fileCache->set("stats_hourly_posts", $mydb->get_stats_hourly_posts(24));
 }
 
-function response_count_last_hour()
-{
+function response_count_last_hour() {
 	global $mydb;
 	$fileCache = new FileCache();
 	$fileCache->set("response_count_last_hour", $mydb->get_response_count_since(gmmktime() - 3600));
 }
 
-function last_10000_sections()
-{
+function last_10000_sections() {
 	global $mydb;
 	
 	$section_array = get_sections_array();
 	$recent_responses = $mydb->get_responses(10000);
 	
 	// might not get these due to an sql error
-	if ($section_array && $recent_responses)
-	{
+	if ($section_array && $recent_responses) {
 		$data_array = process_sections_responses_into_data($recent_responses, $section_array);
 		$fileCache = new FileCache();
 		$fileCache->set("last_10000_sections", $data_array);
 	}
 }
 
-function delete_old_usertokens()
-{
+function delete_old_usertokens() {
 	global $mydb;
+	
 	// delete tokens older than 90 days
 	$mydb->remove_old_token(gmmktime() - 7776000);
 }
 
-function rebuild_sitemap()
-{
+function rebuild_sitemap() {
 	global $default_terms_array;
 	
-	
 	// get all the pages into an array
-	$sitemap = array (
+	$sitemap = array(
 		1 => array(
 			"loc" => "http://rollerderbytestomatic.com",
+			
 			//"lastmod" => "2005-01-01",
 			"changefreq" => "weekly",
 			"priority" => "1"
-		),
+		) ,
 		
 		2 => array(
 			"loc" => "http://rollerderbytestomatic.com/about",
+			
 			//"lastmod" => "2005-01-01",
 			"changefreq" => "weekly",
 			"priority" => "1"
-		),
+		) ,
 		
 		3 => array(
 			"loc" => "http://rollerderbytestomatic.com/stats",
+			
 			//"lastmod" => "2005-01-01",
 			"changefreq" => "weekly",
 			"priority" => "1"
-		),
+		) ,
 		
 		4 => array(
 			"loc" => "http://rollerderbytestomatic.com/stats",
+			
 			//"lastmod" => "2005-01-01",
 			"changefreq" => "weekly",
 			"priority" => "1"
-		),
+		) ,
 		
 		5 => array(
 			"loc" => "http://rollerderbytestomatic.com/test",
+			
 			//"lastmod" => "2005-01-01",
 			"changefreq" => "weekly",
 			"priority" => "1"
@@ -189,10 +191,9 @@ function rebuild_sitemap()
 	
 	$all_questions = get_questions($default_terms_array);
 	
-	foreach ($all_questions as $question)
-	{
+	foreach ($all_questions as $question) {
 		$sitemap[] = array(
-			"loc" => $question->get_URL(),
+			"loc" => $question->get_URL() ,
 			"priority" => "0.5"
 		);
 	}
@@ -202,61 +203,55 @@ function rebuild_sitemap()
 	$sitemap_string = '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 	
-	foreach ($sitemap as $sitemap_item)
-	{
-		$sitemap_string .= '
+	foreach ($sitemap as $sitemap_item) {
+		$sitemap_string.= '
 		<url>';
 		
-		if ($sitemap_item['loc'])
-		{
-			$sitemap_string .= '
+		if ($sitemap_item['loc']) {
+			$sitemap_string.= '
 				<loc>' . htmlentities($sitemap_item['loc']) . "</loc>";
 		}
 		
-		if ($sitemap_item['lastmod'])
-		{
-			$sitemap_string .= '
+		if ($sitemap_item['lastmod']) {
+			$sitemap_string.= '
 				<lastmod>' . htmlentities($sitemap_item['lastmod']) . "</lastmod>";
 		}
 		
-		if ($sitemap_item['changefreq'])
-		{
-			$sitemap_string .= '
+		if ($sitemap_item['changefreq']) {
+			$sitemap_string.= '
 				<changefreq>' . htmlentities($sitemap_item['changefreq']) . "</changefreq>";
 		}
 		
-		if ($sitemap_item['priority'])
-		{
-			$sitemap_string .= '
+		if ($sitemap_item['priority']) {
+			$sitemap_string.= '
 				<priority>' . htmlentities($sitemap_item['priority']) . "</priority>";
 		}
 		
-		$sitemap_string .= '
+		$sitemap_string.= '
 		</url>';
 	}
 	
-	$sitemap_string .= '</urlset> ';
+	$sitemap_string.= '</urlset> ';
 	
 	// save the file
 	file_put_contents("sitemap.xml", $sitemap_string);
 }
 
-function stats_count_unique_IPs()
-{
+function stats_count_unique_IPs() {
 	global $mydb;
 	$fileCache = new FileCache();
 	$fileCache->set("response_distinct_ip_count", $mydb->get_response_distinct_ip_count());
 }
 
-function archive_responses()
-{
+function archive_responses() {
 	global $mydb;
-	// Insert this block of code at the very top of your page: 
 	
-	$time = microtime(); 
-	$time = explode(" ", $time); 
-	$time = $time[1] + $time[0]; 
-	$start = $time; 
+	// Insert this block of code at the very top of your page:
+	
+	$time = microtime();
+	$time = explode(" ", $time);
+	$time = $time[1] + $time[0];
+	$start = $time;
 	
 	// how old do they have to be to archive?
 	$time_ago = gmmktime() - (5184000);
@@ -264,10 +259,9 @@ function archive_responses()
 	$query = "SELECT * FROM rdtom_responses WHERE Timestamp < '$time_ago' ORDER BY ID ASC LIMIT 1000";
 	$results = $mydb->get_results($query);
 	
-	if ($results)
-	{
-		foreach ($results as $result_array)
-		{
+	if ($results) {
+		foreach ($results as $result_array) {
+			
 			// once clean, add it to the database
 			$query = "	
 				REPLACE INTO rdtom_responses_archive (
@@ -286,54 +280,52 @@ function archive_responses()
 					'" . $mydb->mysql_res($result_array['IP']) . "',
 					'" . $result_array['User_ID'] . "')";
 			
-			$mydb->run_query($query);	
+			$mydb->run_query($query);
 			
 			$query = "DELETE FROM rdtom_responses WHERE ID = " . $result_array['ID'];
 			$mydb->run_query($query);
 			$archive_count++;
 		}
-	}
-	else
-	{
+	} else {
 		echo "Nothing needs archiving.<br />";
+		
 		//die;
+		
 	}
 	
-	// Place this part at the very end of your page 
+	// Place this part at the very end of your page
 	
-	$time = microtime(); 
-	$time = explode(" ", $time); 
-	$time = $time[1] + $time[0]; 
-	$finish = $time; 
-	$totaltime = ($finish - $start); 
+	$time = microtime();
+	$time = explode(" ", $time);
+	$time = $time[1] + $time[0];
+	$finish = $time;
+	$totaltime = ($finish - $start);
 	
-	if ($archive_count)
-	{
+	if ($archive_count) {
 		echo "" . $totaltime . " seconds for " . $archive_count . " items. " . ($totaltime / $archive_count) . " per item. <br />";
-	} 
+	}
 }
 
-function unarchive_responses()
-{
+function unarchive_responses() {
 	global $mydb;
-	// Insert this block of code at the very top of your page: 
 	
-	$time = microtime(); 
-	$time = explode(" ", $time); 
-	$time = $time[1] + $time[0]; 
-	$start = $time; 
+	// Insert this block of code at the very top of your page:
+	
+	$time = microtime();
+	$time = explode(" ", $time);
+	$time = $time[1] + $time[0];
+	$start = $time;
 	
 	// how old do they have to be to archive?
 	$time_ago = gmmktime() - (5184000);
 	
 	$query = "SELECT * FROM rdtom_responses_archive WHERE Timestamp > '$time_ago' LIMIT 10";
-
+	
 	$results = $mydb->get_results($query);
 	
-	if ($results)
-	{
-		foreach ($results as $result_array)
-		{
+	if ($results) {
+		foreach ($results as $result_array) {
+			
 			// once clean, add it to the database
 			$query = "	
 				REPLACE INTO rdtom_responses (
@@ -352,30 +344,29 @@ function unarchive_responses()
 					'" . $mydb->mysql_res($result_array['IP']) . "',
 					'" . $result_array['User_ID'] . "')";
 			
-			$mydb->run_query($query);	
+			$mydb->run_query($query);
 			
 			$query = "DELETE FROM rdtom_responses_archive WHERE ID = " . $result_array['ID'];
 			$mydb->run_query($query);
 			$archive_count++;
 		}
-	}
-	else
-	{
+	} else {
 		echo "Nothing needs archiving.<br />";
+		
 		//die;
+		
 	}
 	
-	// Place this part at the very end of your page 
+	// Place this part at the very end of your page
 	
-	$time = microtime(); 
-	$time = explode(" ", $time); 
-	$time = $time[1] + $time[0]; 
-	$finish = $time; 
-	$totaltime = ($finish - $start); 
+	$time = microtime();
+	$time = explode(" ", $time);
+	$time = $time[1] + $time[0];
+	$finish = $time;
+	$totaltime = ($finish - $start);
 	
-	if ($archive_count)
-	{
+	if ($archive_count) {
 		echo "" . $totaltime . " seconds for " . $archive_count . " items. " . ($totaltime / $archive_count) . " per item. <br />";
-	} 
+	}
 }
 ?>
