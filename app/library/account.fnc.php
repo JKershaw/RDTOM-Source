b9dae6de0123e839151083886ddda3101da32a36
Sausage <wardrox@gmail.com>
2014-09-30 18:08:28 +0100

    refactoring

--- app/library/account.fnc.php
+++ app/library/account.fnc.php
@@ -1,7 +1,6 @@
 <?php
 include ("CookieTokenHandler");
 include ("Session");
-include ("Email");
 
 function user_log_in($req_username, $req_password, $rememberMe = false) {
 	global $mydb;
@@ -176,38 +175,6 @@ function is_logged_in() {
 	}
 	
 	return false;
-}
-
-function set_up_reset_token($forgetful_user) {
-	global $mydb;
-	$email = new Email();
-	
-	// has a token been set up in the last 5 mins? If so, error
-	
-	// generate token
-	$token_string = generatealphaneumericSalt(50);
-	
-	// save in the database
-	$mydb->set_password_reset_token($token_string, $forgetful_user->get_ID() , $forgetful_user->get_Email() , get_ip());
-	
-	// email the user
-	$reset_link = get_site_URL() . "passwordreset/" . $token_string;
-	
-	$email_subject = "Roller Derby Test O'Matic password reset";
-	$email_body = "Hello, <br />
-	<br />
-	To reset your Roller Derby Test O'Matic account (your log-in name is " . $forgetful_user->get_Name() . ") password, go to the following URL:<br />
-	<br />
-	<a href='$reset_link'>" . $reset_link . "</a> <br />
-	<br />
-	You can either click the link, or copy the URL into your browser's address bar.<br />
-	<br />
-	If you didn't request to have your password reset then you can ignore this email. If you get this email a bunch of times then something is probably not right. If you're concerned about your account's security, please get in touch via contact@rollerderbytestomatic.com.";
-	
-	$email->send($forgetful_user->get_Email() , $email_subject, $email_body);
-	
-	// save a log
-	save_log("password_reset", "User Email: " . $forgetful_user->get_Email());
 }
 
 function set_up_logged_in_user() {
--- app/presentation/passwordreset.php
+++ app/presentation/passwordreset.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
  * 1. User gives email of forgotten password account
@@ -7,103 +7,81 @@
  *    - Email sent including a token
  * 3. User has followed a link with a token in, if token is valid user prompted to enter a new password (twice, to check), token included in the form
  * 4. Token is correct, password is updated and the user is told to log in, email sent to user saying the password has been reset.
- * 
+ *
  * 1. /passwordreset
  * 2. /passwordreset $_POST['forgottenemailform']
  * 3. /passwordreset/[code]
  * 4. /passwordreset/[code] $_POST['forgottenemailnewpasswordform']
- * 
- */
+ *
+*/
 
-if ($url_array[1])
-{
+if ($url_array[1]) {
+	
 	// is the token valid?
-	if (!$mydb->is_valid_password_reset_token($url_array[1]))
-	{
+	if (!$mydb->is_valid_password_reset_token($url_array[1])) {
 		$error_string = "The link you followed is not valid.";
 		$url_array[1] = false;
 	}
 }
 
 // process the range of forms which may be submitted
-if (!$error_string && ($_POST['forgottenemailform'] == "yes"))
-{
-	if (is_logged_in())
-	{
+if (!$error_string && ($_POST['forgottenemailform'] == "yes")) {
+	if (is_logged_in()) {
 		$error_string = "You need to log out before you can request a password reset.";
-	}
-	else 
-	{
-
-			// step 2
-			
-			// fetch the forgetful user, will throw error if user not found
-			
-			// is the thing used an email address?
-			if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL ))
-			{
-                try
-                {
-				    $forgetful_user = $mydb->get_user_from_email($_POST['email']);
-                }
-                catch (Exception $e)
-                {
-                    $error_string = "Sorry, that email address is not associated with any account. Please try again.";
-                }
+	} else {
+		
+		// step 2
+		
+		// fetch the forgetful user, will throw error if user not found
+		
+		// is the thing used an email address?
+		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			try {
+				$forgetful_user = $mydb->get_user_from_email($_POST['email']);
 			}
-			else
-			{
-                try
-                {
-                    $forgetful_user = $mydb->get_user_from_name($_POST['email']);
-                }
-                catch (Exception $e)
-                {
-                    $error_string = "Sorry, that name is not associated with any account. Please try again.";
-                }
+			catch(Exception $e) {
+				$error_string = "Sorry, that email address is not associated with any account. Please try again.";
 			}
-
-			// function to save the reset token in the database & email the user
-        if($forgetful_user)
-        {
-            try
-            {
-                set_up_reset_token($forgetful_user);
-            }
-            catch (Exception $e)
-            {
-                $error_string = "Sorry, we were not able to send you an email with your password rest info in. This could be caused by you not registering an email address on sign up (and, to be fair, the email bit is optional). If you think your account might not have an email address associated, send a message to contact@rollerderbytestomatic.com and we can sort it out.";
-            }
-        }
+		} else {
+			try {
+				$forgetful_user = $mydb->get_user_from_name($_POST['email']);
+			}
+			catch(Exception $e) {
+				$error_string = "Sorry, that name is not associated with any account. Please try again.";
+			}
+		}
+		
+		// function to save the reset token in the database & email the user
+		if ($forgetful_user) {
+			try {
+				set_up_reset_token($forgetful_user);
+			}
+			catch(Exception $e) {
+				$error_string = "Sorry, we were not able to send you an email with your password rest info in. This could be caused by you not registering an email address on sign up (and, to be fair, the email bit is optional). If you think your account might not have an email address associated, send a message to contact@rollerderbytestomatic.com and we can sort it out.";
+			}
+		}
 	}
 }
 
-if (!$error_string && ($_POST['forgottenemailnewpasswordform'] == "yes"))
-{
-	if (is_logged_in())
-	{
+if (!$error_string && ($_POST['forgottenemailnewpasswordform'] == "yes")) {
+	if (is_logged_in()) {
 		$error_string = "You need to log out before you can request a password reset.";
-	}
-	else 
-	{
-		try 
-		{
+	} else {
+		try {
+			
 			// step 4
 			// is the password valid?
-			if ($_POST['password'] != $_POST['password2'])
-			{
+			if ($_POST['password'] != $_POST['password2']) {
 				throw new exception("The two passwords did not match, please try again.");
 			}
 			
-			if (!$mydb->is_valid_password_reset_token($_POST['token']))
-			{
+			if (!$mydb->is_valid_password_reset_token($_POST['token'])) {
 				$error_string = "The link you followed is not valid.";
 			}
 			
 			// is the password valid
-			if (strlen($_POST['password']) < 8)
-			{
-				throw new exception ("You need to have a password which is 8 or more characters long.");
+			if (strlen($_POST['password']) < 8) {
+				throw new exception("You need to have a password which is 8 or more characters long.");
 			}
 			
 			// ALL VALID!
@@ -113,31 +91,32 @@ if (!$error_string && ($_POST['forgottenemailnewpasswordform'] == "yes"))
 			$mydb->use_password_reset_token($_POST['token']);
 			
 			// update the password
-			$mydb->set_user_password($forgetful_user->get_ID(), $_POST['password']);
-			
+			$mydb->set_user_password($forgetful_user->get_ID() , $_POST['password']);
 		}
-		catch (Exception $e) 
-		{
+		catch(Exception $e) {
 			$error_string = $e->getMessage();
 		}
 	}
 }
+
 // show the page
 
 set_page_subtitle("Turn left and forget your password.");
-include("header.php"); 
+include ("header.php");
 
-if (!$url_array[1])
-{
+if (!$url_array[1]) {
+	
 	// step 1 or 2
-	if (($_POST['forgottenemailform'] != "yes") || $error_string)
-	{
+	if (($_POST['forgottenemailform'] != "yes") || $error_string) {
+		
 		// step 1
-		?>
+		
+?>
 
 		<h3>To have your password reset, please enter your email address or user name. You'll be emailed details on what to do next.</h3>
 		
-		<form method="post" action="<?php echo get_site_URL(); ?>passwordreset" name="formforgottenemail">
+		<form method="post" action="<?php
+		echo get_site_URL(); ?>passwordreset" name="formforgottenemail">
 			<input type="hidden"  name="forgottenemailform" id="forgottenemailform" value="yes">
 			<p>
 				Email or Name:<br />
@@ -148,31 +127,34 @@ if (!$url_array[1])
 				<a class="button" onClick="document.formforgottenemail.submit()">Reset Password</a>
 			</p>
 		</form>
-		<?php 
-	}
-	else
-	{
+		<?php
+	} else {
+		
 		// step 2
-		?>
+		
+?>
 		<h3>An email has been sent with instructions on how to reset your password.</h3>
 		
 		<p>The email will be valid for the next 24 hours. After this time if you've not reset your password you'll need to enter your email address again and have a new email sent.</p>
-		<?php 
-	}	
-}
-else
-{
+		<?php
+	}
+} else {
+	
 	// step 3 or 4
-	if (($_POST['forgottenemailnewpasswordform'] != "yes") || $error_string)
-	{
+	if (($_POST['forgottenemailnewpasswordform'] != "yes") || $error_string) {
+		
 		// step 3
 		// $url_array[1] is the password reset token
-		?>
+		
+?>
 		<h3>Please enter your new password:</h3>
 		
-		<form method="post" action="<?php echo get_site_URL(); ?>passwordreset/<?php echo $url_array[1]; ?>" name="formforgottenemailnewpassword">
+		<form method="post" action="<?php
+		echo get_site_URL(); ?>passwordreset/<?php
+		echo $url_array[1]; ?>" name="formforgottenemailnewpassword">
 			<input type="hidden" name="forgottenemailnewpasswordform" id="forgottenemailnewpasswordform" value="yes">
-			<input type="hidden" name="token" id="token" value="<?php echo $url_array[1]; ?>">
+			<input type="hidden" name="token" id="token" value="<?php
+		echo $url_array[1]; ?>">
 			<p>
 				Password:<br />
 				<input class="input_text" type="password" id="password" name ="password">
@@ -186,18 +168,19 @@ else
 				<a class="button" onClick="document.formforgottenemailnewpassword.submit()">Save Password</a>
 			</p>
 		</form>
-		<?php 
-	}
-	else
-	{
+		<?php
+	} else {
+		
 		// step 4
-		?>
+		
+?>
 		<h3>Your password has been reset!</h3>
 
-		<p>Hurray! You can <a href="<?php echo get_site_URL(); ?>profile">now log in</a> with your new password.</p>
-		<?php 
+		<p>Hurray! You can <a href="<?php
+		echo get_site_URL(); ?>profile">now log in</a> with your new password.</p>
+		<?php
 	}
 }
 
-include("footer.php"); 
+include ("footer.php");
 ?>
