6901672ddf38febbb575199a95272b02640bef35
Sausage <wardrox@gmail.com>
2014-10-03 15:46:03 +0100

    refactoring

--- app/library/forum.fnc.php
+++ app/library/forum.fnc.php
@@ -934,3 +934,29 @@ function get_gravatar($email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts 
 	}
 	return $url;
 }
+
+// taken from http://www.zachstronaut.com/posts/2009/01/20/php-relative-date-time-string.html
+function time_elapsed_string($ptime) {
+	$etime = gmmktime() - $ptime;
+	
+	if ($etime < 10) {
+		return 'just now';
+	}
+	
+	$a = array(
+		12 * 30 * 24 * 60 * 60 => 'year',
+		30 * 24 * 60 * 60 => 'month',
+		24 * 60 * 60 => 'day',
+		60 * 60 => 'hour',
+		60 => 'minute',
+		1 => 'second'
+	);
+	
+	foreach ($a as $secs => $str) {
+		$d = $etime / $secs;
+		if ($d >= 1) {
+			$r = round($d);
+			return $r . ' ' . $str . ($r > 1 ? 's' : '') . " ago";
+		}
+	}
+}
