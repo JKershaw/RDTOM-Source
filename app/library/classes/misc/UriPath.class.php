380c4945aa2a677c3f778f57be15518dbeebb2eb
Sausage <wardrox@gmail.com>
2014-10-07 18:55:13 +0100

    Uri is case insensitive

--- app/library/classes/misc/UriPath.class.php
+++ app/library/classes/misc/UriPath.class.php
@@ -14,6 +14,6 @@ class UriPath
 		}
 
 		$segments = explode("/", $path);
-		return $segments[$number];
+		return strtolower($segments[$number]);
 	}
 }
--- tests/unit/UriPathTest.php
+++ tests/unit/UriPathTest.php
@@ -24,6 +24,7 @@ class UriPathTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals("bar", UriPath::part(1));
 		$this->assertEquals("choo", UriPath::part(2));
 	}
+
 	public function testUriPathWithKeys() {
 
 		$_SERVER['REQUEST_URI'] = "foo/?rawr=ohnoes";
@@ -41,5 +42,17 @@ class UriPathTest extends \PHPUnit_Framework_TestCase
 		$_SERVER['REQUEST_URI'] = "foo/bar?rawr=ohnoes";
 		$this->assertEquals("foo", UriPath::part(0));
 		$this->assertEquals("bar", UriPath::part(1));
+	}
+	public function testUriPathWithCapitals() {
+
+
+		$_SERVER['REQUEST_URI'] = "FOO/";
+		$this->assertEquals("foo", UriPath::part(0));
+		$this->assertEquals("foo", UriPath::part());
+
+		$_SERVER['REQUEST_URI'] = "foo/Bar/chOO";
+		$this->assertEquals("foo", UriPath::part(0));
+		$this->assertEquals("bar", UriPath::part(1));
+		$this->assertEquals("choo", UriPath::part(2));
 	}
 }
